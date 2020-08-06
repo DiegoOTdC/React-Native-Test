d3a9bc919fa4a8b699b4e2234b5b2876915e2552
DiegoOTdC <studioartdiego@gmail.com>
2020-08-06 22:36:36 +0200

    colors change on motion sensors

--- App.js
+++ App.js
@@ -1,29 +1,7 @@
 import React from "react";
 import { View, StyleSheet } from "react-native";
+import GameScreen from "./screens/GameScreen";
 
 export default function App() {
-  return (
-    <View style={styles.row}>
-      <View style={styles.purple} />
-      <View style={styles.crimson} />
-      <View style={styles.gold} />
-    </View>
-  );
+  return <GameScreen />;
 }
-
-const styles = StyleSheet.create({
-  purple: {
-    width: "20%",
-    height: "20%",
-    backgroundColor: "rebeccapurple",
-  },
-  crimson: { width: "50%", height: "20%", backgroundColor: "crimson" },
-  gold: { width: "80%", height: "20%", backgroundColor: "gold" },
-  row: {
-    height: "100%",
-    flexDirection: "column",
-    alignItems: "center",
-    justifyContent: "space-between",
-    justifyContent: "space-around",
-  },
-});
